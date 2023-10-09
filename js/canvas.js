98c6a37005de4b9f3480a76803600f056c0a4373
Daniel Knott <danielgknott@gmail.com>
2023-10-08 20:05:39 -0400

    Canvas Game Movement
    - Added in Button Movement
    - Adding Game Piece

--- js/canvas.js
+++ js/canvas.js
@@ -1,4 +1,7 @@
+var myGamePiece;
+
 function startGame(){
+    myGamePiece = new component(30, 30, "red", 10, 120);
     myGameArea.start();
 }
 
@@ -10,5 +13,52 @@ let myGameArea = {
         this.context = this.canvas.getContext("2d");
         let containerDiv = document.getElementById("game-canvas");
         containerDiv.appendChild(this.canvas);
+        this.interval = setInterval(updateGameArea, 20);
+    },
+    clear : function(){
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
 }
+
+//Contruct for Game Piece Object Creation
+function component(width, height, color, x, y) {
+    this.width = width;
+    this.height = height;
+    this.speedX = 0;
+    this.speedY = 0;
+    this.x = x;
+    this.y = y;
+    this.update = function() {
+        ctx = myGameArea.context;
+        ctx.fillStyle = color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+    this.newPos = function() {
+        this.x += this.speedX;
+        this.y += this.speedY;        
+    }    
+}
+
+//Creating the frames per second by clearing and updating functions
+function updateGameArea() {
+    myGameArea.clear();
+    myGamePiece.newPos();
+    myGamePiece.update();
+}
+
+function moveup() {
+    myGamePiece.speedY -= 1;
+}
+
+function movedown() {
+    myGamePiece.speedY += 1;
+}
+
+function moveleft() {
+    myGamePiece.speedX -= 1;
+}
+
+function moveright() {
+    myGamePiece.speedX += 1;
+}
+
