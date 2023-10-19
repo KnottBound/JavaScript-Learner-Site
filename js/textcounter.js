46f41a7e4a576b74079db3d5b97968cd4cd968dd
KnottBound <danielgknott@gmail.com>
2023-10-18 23:00:03 -0400

    Word Counter Now
    - Original made it count characters, but what we needed was a word counter.

--- js/textcounter.js
+++ js/textcounter.js
@@ -1,17 +1,29 @@
-const textareaEl = document.getElementById('textarea');
+//const textareaEl = document.getElementById('textarea');
 const totalCounterEl = document.getElementById('total-counter');
 const invoiceCounterEl = document.getElementById('invoice-counter');
 
-textareaEl.addEventListener("keyup", ()=>{
-    updateCounter();
-    updateInvoicePrice();
+function countWords() {
+    let text = document.getElementById('textarea').value;
 
-});
+    let numWords = 0;
 
-function updateCounter(){
-    totalCounterEl.innerText = textareaEl.value.length;
-}
+    for (let i = 0; i< text.length; i++){
+        let currentCharacter = text[i];
 
-function updateInvoicePrice(){
-    invoiceCounterEl.innerText = "$" + textareaEl.value.length * .41;
+        if(currentCharacter == " ") {
+            numWords += 1;
+            updateCounter();
+            updateInvoicePrice();
+        }
+    }
+
+    numWords += 1;
+
+    function updateCounter(){
+        totalCounterEl.innerText = numWords;
+    }
+
+    function updateInvoicePrice(){
+        invoiceCounterEl.innerText = "$" + numWords * .41;
+    }
 }
