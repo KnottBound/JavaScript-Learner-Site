8e98e8fde8d476a1d2f37d8fe3f16675ef3e66b8
KnottBound <danielgknott@gmail.com>
2023-10-21 23:40:08 -0400

    More Useful Fetch
    - Added response so that only information I want shows

---
+++ js/apifetch.js
@@ -0,0 +1,34 @@
+document.getElementById('getDataButton').addEventListener('click', fetchData);
+
+function fetchData() {
+    const searchQuery = document.getElementById('searchQueryInput').value;
+    const apiUrl = `http://127.0.0.1:5000/position/${searchQuery}`;
+
+    fetch(apiUrl)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            // API response data
+            const organicResults = data.organic_results;
+
+
+            //const dataContainer = document.getElementById('dataContainer');
+            //dataContainer.innerHTML = JSON.stringify(data, null, 2);
+            const resultHTML = organicResults.map(result => `
+            <div style="color:white;">
+                <h2>${result.title}</h2>
+                <p>${result.snippet}</p>
+                <a href="${result.link}" target="_blank">${result.link}</a>
+            </div>
+        `).join('');
+        dataContainer.innerHTML = resultHTML;
+        })
+        .catch(error => {
+            // Errors
+            console.error('API Error:', error);
+        });
+}
--- js/fetch.js
+++ js/fetch.js
@@ -1,31 +1,31 @@
-document.getElementById('getDataButton').addEventListener('click', fetchData);
+// Get references to HTML elements
+const searchInput = document.getElementById('searchInput');
+const searchButton = document.getElementById('searchButton');
+const dataContainer = document.getElementById('dataContainer');
 
-        function fetchData() {
-            const searchQuery = document.getElementById('searchQueryInput').value;
-            const apiUrl = `http://127.0.0.1:5000/position/${searchQuery}`;
+// Define a function to fetch and display data
+function fetchData() {
+    const query = searchInput.value;
+    const apiUrl = `http://127.0.0.1:5000/position/?q=${query}`; // Replace with your API endpoint
 
-            fetch(apiUrl)
-                .then(response => {
-                    if (!response.ok) {
-                        throw new Error('Network response was not ok');
-                    }
-                    return response.json();
-                })
-                .then(data => {
-                    // API response data
-                    const dataContainer = document.getElementById('dataContainer');
-                    dataContainer.innerHTML = JSON.stringify(data, null, 2);
-                    const resultHTML = organicResults.map(result => `
-                    <div>
-                        <h2>${result.title}</h2>
-                        <p>${result.snippet}</p>
-                        <a href="${result.link}" target="_blank">${result.link}</a>
-                    </div>
-                `).join('');
-                dataContainer.innerHTML = resultHTML;
-                })
-                .catch(error => {
-                    // Errors
-                    console.error('API Error:', error);
-                });
-            }
+    // Fetch data from the API
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            // Extract and display organic results
+            const organicResults = data.organic_results;
+            dataContainer.innerHTML = ''; // Clear previous results
+            organicResults.forEach(result => {
+                const { title, snippet, link } = result;
+                const resultElement = document.createElement('div');
+                resultElement.innerHTML = `<h3>${title}</h3><p>${snippet}</p><a href="${link}" target="_blank">Read more</a>`;
+                dataContainer.appendChild(resultElement);
+            });
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
+}
+
+// Add click event listener to the Search button
+searchButton.addEventListener('click', fetchData);
